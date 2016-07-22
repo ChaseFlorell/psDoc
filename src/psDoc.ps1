020ad68620808234f60a0e165e702eb5f90827d1
Dan Nixon <dan@dan-nixon.com>
2016-07-22 10:47:25 +0100

    Create the output directory if it does not exist

--- src/psDoc.ps1
+++ src/psDoc.ps1
@@ -1,15 +1,15 @@
 param(
-    [parameter(Mandatory=$true, Position=0)] [string] $moduleName, 
+    [parameter(Mandatory=$true, Position=0)] [string] $moduleName,
     [parameter(Mandatory=$false, Position=1)] [string] $template = "./out-html-template.ps1",
-    [parameter(Mandatory=$false, Position=2)] [string] $outputDir = './help', 
+    [parameter(Mandatory=$false, Position=2)] [string] $outputDir = './help',
     [parameter(Mandatory=$false, Position=3)] [string] $fileName = 'index.html'
 )
 
 function FixString ($in = '', [bool]$includeBreaks = $false){
     if ($in -eq $null) { return }
-    
+
     $rtn = $in.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;').Trim()
-    
+
     if($includeBreaks){
         $rtn = $rtn.Replace([Environment]::NewLine, '<br>')
     }
@@ -27,14 +27,14 @@ foreach ($h in $commandsHelp){
 
     # Get any aliases associated with the method
     $alias = get-alias -definition $h.Name -ErrorAction SilentlyContinue
-    if($alias){ 
+    if($alias){
         $h | Add-Member Alias $alias
     }
-    
+
     # Parse the related links and assign them to a links hashtable.
     if(($h.relatedLinks | Out-String).Trim().Length -gt 0) {
         $links = $h.relatedLinks.navigationLink | % {
-            if($_.uri){ @{name = $_.uri; link = $_.uri; target='_blank'} } 
+            if($_.uri){ @{name = $_.uri; link = $_.uri; target='_blank'} }
             if($_.linkText){ @{name = $_.linkText; link = "#$($_.linkText)"; cssClass = 'psLink'; target='_top'} }
         }
         $h | Add-Member Links $links
@@ -47,6 +47,11 @@ foreach ($h in $commandsHelp){
             $p | Add-Member Aliases "$($paramAliases -join ', ')" -Force
         }
     }
+}
+
+# Create the output directory if it does not exist
+if (-Not (Test-Path $outputDir)) {
+    New-Item -Path $outputDir -ItemType Directory
 }
 
 $totalCommands = $commandsHelp.Count
