d09310bc7e50d7da5a86ac1604ffc57921400512
Sukru Alatas <alatas@gmail.com>
2018-02-06 00:34:45 +0300

    markdown fixes and changes - converted to native md tables - changed clear line breaks - added gt and lt replacements - added syntax highlight - fixed links problem

--- src/out-markdown-template.ps1
+++ src/out-markdown-template.ps1
@@ -1,174 +1,159 @@
 function TrimAllLines([string] $str) {
-	$lines = $str -split "`n"
+  $lines = $str -split "`n"
 
-	for ($i = 0; $i -lt $lines.Count; $i++) {
-		$lines[$i] = $lines[$i].Trim()
-	}
+  for ($i = 0; $i -lt $lines.Count; $i++) {
+    $lines[$i] = $lines[$i].Trim()
+  }
 
-	# Trim EOL.
-	($lines | Out-String).Trim()
+  # Trim EOL.
+  ($lines | Out-String).Trim()
 }
 
 function FixMarkdownString([string] $in = '', [bool] $includeBreaks = $false) {
-	if ($in -eq $null) { return }
+  if ($in -eq $null) { return }
 
-	$replacements = @{
-		'\' = '\\'
-		'`' = '\`'
-		'*' = '\*'
-		'_' = '\_'
-		'{' = '\{'
-		'}' = '\}'
-		'[' = '\['
-		']' = '\]'
-		'(' = '\('
-		')' = '\)'
-		'#' = '\#'
-		'+' = '\+'
-		'!' = '\!'
-	}
+  $replacements = @{
+    '\' = '\\'
+    '`' = '\`'
+    '*' = '\*'
+    '_' = '\_'
+    '{' = '\{'
+    '}' = '\}'
+    '[' = '\['
+    ']' = '\]'
+    '(' = '\('
+    ')' = '\)'
+    '#' = '\#'
+    '+' = '\+'
+    '!' = '\!'
+    '<' = '\<'
+    '>' = '\>'
+  }
 
-	$rtn = $in.Trim()
-	foreach ($key in $replacements.Keys) {
-		$rtn = $rtn.Replace($key, $replacements[$key])
-	}
+  $rtn = $in.Trim()
+  foreach ($key in $replacements.Keys) {
+    $rtn = $rtn.Replace($key, $replacements[$key])
+  }
 
-	$rtn = TrimAllLines $rtn
-
-	if ($includeBreaks) {
-		$crlf = [Environment]::NewLine
-		$rtn = $rtn.Replace($crlf, "  $crlf")
-	}
-	$rtn
+  $rtn = TrimAllLines $rtn
+  $crlf = [Environment]::NewLine
+  if ($includeBreaks) {
+    $rtn = $rtn.Replace($crlf, "  $crlf")
+  }
+  else {
+    $rtn = $rtn.Replace($crlf, " ").Trim()
+  }
+  $rtn
 }
 
 function FixMarkdownCodeString([string] $in) {
-	if ($in -eq $null) { return }
+  if ($in -eq $null) { return }
 	
-	TrimAllLines $in
+  TrimAllLines $in
 }
 
 @"
-# $moduleName
+# $moduleName Module
 "@
 $progress = 0
 $commandsHelp | % {
-	Update-Progress $_.Name 'Documentation'
-	$progress++
-@"
+  Update-Progress $_.Name 'Documentation'
+  $progress++
+  @"
 ## $(FixMarkdownString($_.Name))
 "@
-	$synopsis = $_.synopsis.Trim()
-	$syntax = $_.syntax | out-string
-	if(-not ($synopsis -ilike "$($_.Name.Trim())*")){
-		$tmp = $synopsis
-		$synopsis = $syntax
-		$syntax = $tmp
-@"	
+  $synopsis = $_.synopsis.Trim()
+  $syntax = $_.syntax | out-string
+  if (-not ($synopsis -ilike "$($_.Name.Trim())*")) {
+    $tmp = $synopsis
+    $synopsis = $syntax
+    $syntax = $tmp
+    @"	
 ### Synopsis
 $(FixMarkdownString($syntax))
 "@
-	}
-@"	
+  }
+  @"	
 ### Syntax
-$(FixMarkdownString($synopsis))
+``````powershell
+$($synopsis)
+``````
 "@	
 
-	if (!($_.alias.Length -eq 0)) {
-@"
+  if (!($_.alias.Length -eq 0)) {
+    @"
 ### $($_.Name) Aliases
 "@
-		$_.alias | % {
-@"
+    $_.alias | % {
+      @"
  - $($_.Name)
 "@
-		}
-@"
+    }
+    @"
 
 "@
-	}
+  }
 	
-	if($_.parameters){
-@"
+  if ($_.parameters) {
+    @"
 ### Parameters
-
-<table class="table table-striped table-bordered table-condensed visible-on">
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th class="visible-lg visible-md">Alias</th>
-			<th>Description</th>
-			<th class="visible-lg visible-md">Required?</th>
-			<th class="visible-lg">Pipeline Input</th>
-			<th class="visible-lg">Default Value</th>
-		</tr>
-	</thead>
-	<tbody>
+| Name  | Alias  | Description | Required? | Pipeline Input | Default Value |
+| - | - | - | - | - | - |
 "@
-		$_.parameters.parameter | % {
-@"
-		<tr>
-			<td><nobr>$(FixMarkdownString($_.Name))</nobr></td>
-			<td class="visible-lg visible-md">$(FixMarkdownString($_.Aliases))</td>
-			<td>$(FixMarkdownString(($_.Description  | out-string).Trim()) $true)</td>
-			<td class="visible-lg visible-md">$(FixMarkdownString($_.Required))</td>
-			<td class="visible-lg">$(FixMarkdownString($_.PipelineInput))</td>
-			<td class="visible-lg">$(FixMarkdownString($_.DefaultValue))</td>
-		</tr>
+    $_.parameters.parameter | % {
+      @"
+| <nobr>$(FixMarkdownString($_.Name))</nobr> | $(FixMarkdownString($_.Aliases)) | $(FixMarkdownString(($_.Description  | out-string).Trim())) | $(FixMarkdownString($_.Required)) | $(FixMarkdownString($_.PipelineInput)) | $(FixMarkdownString($_.DefaultValue)) |
 "@
-		}
-@"
-	</tbody>
-</table>			
-"@
-	}
-	$inputTypes = $(FixMarkdownString($_.inputTypes  | out-string))
-	if ($inputTypes.Length -gt 0 -and -not $inputTypes.Contains('inputType')) {
-@"
+    }
+  }
+  $inputTypes = $(FixMarkdownString($_.inputTypes  | out-string))
+  if ($inputTypes.Length -gt 0 -and -not $inputTypes.Contains('inputType')) {
+    @"
 ### Inputs
  - $inputTypes
 
 "@
-	}
-	$returnValues = $(FixMarkdownString($_.returnValues  | out-string))
-	if ($returnValues.Length -gt 0 -and -not $returnValues.StartsWith("returnValue")) {
-@"
+  }
+  $returnValues = $(FixMarkdownString($_.returnValues  | out-string))
+  if ($returnValues.Length -gt 0 -and -not $returnValues.StartsWith("returnValue")) {
+    @"
 ### Outputs
  - $returnValues
 
 "@
-	}
-	$notes = $(FixMarkdownString($_.alertSet  | out-string))
-	if ($notes.Trim().Length -gt 0) {
-@"
+  }
+  $notes = $(FixMarkdownString($_.alertSet  | out-string))
+  if ($notes.Trim().Length -gt 0) {
+    @"
 ### Note
 $notes
 
 "@
-	}
-	if(($_.examples | Out-String).Trim().Length -gt 0) {
-@"
+  }
+  if (($_.examples | Out-String).Trim().Length -gt 0) {
+    @"
 ### Examples
 "@
-		$_.examples.example | % {
-@"
+    $_.examples.example | % {
+      @"
 **$(FixMarkdownString($_.title.Trim(('-',' '))))**
-
-		$(FixMarkdownCodeString($_.code | out-string ))
-		
+``````powershell
+$(FixMarkdownCodeString($_.code | out-string ))
+``````
 $(FixMarkdownString($_.remarks | out-string ) $true)
+
 "@
-		}
-	}
-	if(($_.relatedLinks | Out-String).Trim().Length -gt 0) {
-@"
+    }
+  }
+  if (($_.relatedLinks | Out-String).Trim().Length -gt 0) {
+    @"
 ### Links
 
 "@
-		$_.links | % { 
-@"
- - [$_.name]($_.link)
+    $_.links | % { 
+      @"
+ - [$($_.name)]($($_.link))
 "@
-		}
-	}
+    }
+  }
 }
